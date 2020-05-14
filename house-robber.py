04ce7ef5714f7e1067726c1038be4f7f9ad5e0b0
ssong10 <leesy1403@naver.com>
2020-05-15 00:18:30 +0900

    house-robber

--- house-robber.py
+++ house-robber.py
@@ -1,11 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
         if len(nums) < 3:
             return max(nums)
         no_rob = [0] * len(nums)
         rob = [0] * len(nums)
         for i in range(len(nums)):
-            no_rob[i] = rob[i-2] + nums[i]
-            rob[i] = max(no_rob[i-1],rob[i-2]) + nums[i]
-        print(no_rob, rob)
-        return max(rob)
+            no_rob[i] = max(rob[i-1], no_rob[i-2]+nums[i])
+            rob[i] = max(no_rob[i-2],rob[i-2]) + nums[i]
+        return max(rob[-2:])
