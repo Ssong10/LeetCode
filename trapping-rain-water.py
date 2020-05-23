c9331341ed3905f0a9b5baa8360fa096b9979526
ssong10 <leesy1403@naver.com>
2020-05-23 22:04:36 +0900

    trapping-rain-water

--- trapping-rain-water.py
+++ trapping-rain-water.py
@@ -1,20 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        heights = [0] * (max(height)+1)
-        if not height:
-            return 0
-        tmp = height[0]
-        result = 0
-        for i in range(len(height)):
-            h = height[i]
-            if tmp < h:
-                x, y =0 , 0
-                for hi in range(tmp+1,h+2):
-                    if x < heights[hi]:
-                        x = heights[hi]
-                        y = hi
-                print(i,i-x,y-tmp)
-                result += (i-x)*(y-tmp)
-            heights[h] = i+1
-            tmp = h
-        return result
+        left,right = 0, len(height)-1
+        ans = 0
+        left_max, right_max = 0, 0
+        while(left < right):
+            if height[left] < height[right]:
+                if height[left] > left_max:
+                    left_max = height[left]
+                else:
+                    ans += left_max - height[left]
+                left = left + 1
+            else:
+                if height[right] > right_max:
+                    right_max = height[right]
+                else:
+                    ans += right_max - height[right]
+                right = right - 1
+        return ans
